c64c93566861fbefc3f1a7361c2e1ee57838f9b9
Emmanuel6093 <elorenzo6093@gmail.com>
2024-06-04 19:40:06 -0400

    creating switch statements for menu option

--- Program.cs
+++ Program.cs
@@ -19,6 +19,28 @@ namespace BankAccountManager
                 Console.WriteLine("5. Exit");
                 Console.WriteLine("Choose an option: ");
                 string choice = Console.ReadLine();
+
+                switch (choice) 
+                {
+                    case "1":
+                        Console.Write("Enter your name: ");
+                        string name = Console.ReadLine();
+                        account.CreateAccount(name);
+                        Console.WriteLine($"Account created for {name}.");
+                        break;
+                    case "2": 
+                        Console.Write("Enter amount to deposit: ");
+                        if (double.TryParse(Console.ReadLine(), out double depositAmount))
+                        {
+                            account.Deposit(depositAmount);
+                            Console.WriteLine($"Deposited {depositAmount:C}.");
+                        }
+                        else 
+                        {
+                            Console.WriteLine("Invalid amount.");
+                        }
+                        break;
+                }
             }
         }
     }
