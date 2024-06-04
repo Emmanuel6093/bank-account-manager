2580ba20c98a099159d8622a102e6829b4e2eb98
Emmanuel6093 <elorenzo6093@gmail.com>
2024-06-04 19:26:38 -0400

    setting up menu option to capture user input

--- Program.cs
+++ Program.cs
@@ -11,7 +11,14 @@ namespace BankAccountManager
             
             while (running)
             {
-                
+                Console.WriteLine("Welcome to the simple Bank Account Manager!");
+                Console.WriteLine("1. Create Account");
+                Console.WriteLine("2. Deposit");
+                Console.WriteLine("3. Withdraw");
+                Console.WriteLine("4. Check Balance");
+                Console.WriteLine("5. Exit");
+                Console.WriteLine("Choose an option: ");
+                string choice = Console.ReadLine();
             }
         }
     }
